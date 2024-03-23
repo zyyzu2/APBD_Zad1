bc2cda475f916b4feaed61467c20b97280df1ae1
zyyzu2 <s24854@pjwstk.edu.pl>
2024-03-23 18:04:17 +0100

    change to static

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ WriteLine("Hello, World45!");
 WriteLine("Hello, World21213135!");
 WriteLine("Hello, World45!");
 
-double CalculateAverage(int[] inst)
+static double CalculateAverage(int[] inst)
 {
     return inst.Average();
 }
